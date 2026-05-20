d33a7779e2fc5f66c1e1a0dcdb896554f165b796
GiannisAnt0 <antoniadisioann@gmail.com>
2026-05-20 22:25:19 +0300

    Update test_sensors.py
    made changes in test_sensors

--- test_sensors.py
+++ test_sensors.py
@@ -4,7 +4,6 @@ import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 from gpiozero import Button
 
-# --- Ρυθμίσεις Αισθητήρα Ροής (YF-S201) ---
 FLOW_SENSOR_PIN = 17
 
 flow_sensor = Button(FLOW_SENSOR_PIN, pull_up=True)
@@ -20,19 +19,19 @@ flow_sensor.when_pressed = count_pulse
 i2c = board.I2C()
 ads = ADS.ADS1015(i2c)
 
-# ΔΙΟΡΘΩΣΗ: Χρησιμοποιούμε το 0 απευθείας αντί για ADS.P0
+
 chan = AnalogIn(ads, 0)
 
-print("🚀 Το τεστ ξεκινάει! Πάτα Ctrl+C για να το σταματήσεις.\n")
+print(" Το τεστ ξεκινάει! Πάτα Ctrl+C για να το σταματήσεις.\n")
 
 try:
     while True:
         raw_value = chan.value
         voltage = chan.voltage
         
-        print(f"💧 Υγρασία (Ακατέργαστη): {raw_value:5d} | Τάση: {voltage:.2f}V  ---  💦 Παλμοί: {pulse_count}")
+        print(f" Υγρασία (Ακατέργαστη): {raw_value:5d} | Τάση: {voltage:.2f}V  ---   Παλμοί: {pulse_count}")
         
         time.sleep(1)
 
 except KeyboardInterrupt:
-    print("\n🛑 Το τεστ σταμάτησε.")
+    print("\n Το τεστ σταμάτησε.")
